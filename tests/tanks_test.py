0f103a1919a33696e94e6f0261e06974c73f7c51
gr24269 <griano@utexas.edu>
2021-01-13 15:06:42 -0600

    Created folder for tests and organized files

--- tests/tanks_test.py
+++ tests/tanks_test.py
@@ -4,11 +4,9 @@ from pprint import pprint
 from collections import defaultdict as ddict
 import pickle
 
-swmm5.initialize('/home/griano/Documents/Github/SWMMwrapper/swmm_files/Brazos_EveryComplexSource.inp')
-
-# [outfall, upstream BC]
-nodes = ['630016861', '630047272']
-links = ['12642'] # fits into the outfall
+swmm5.initialize('/home/griano/Documents/Github/SWMMwrapper/swmm_files/tanks.inp')
+nodes = ['N-5', 'N-6']
+links = ['C-1', 'C-2']
 
 node_results = {n : ddict(list) for n in nodes}
 link_results = {l : ddict(list) for l in links}
@@ -38,7 +36,6 @@ with open('node_results.pkl', 'wb') as f:
 
 with open('link_results.pkl', 'wb') as f:
     pickle.dump({'time': time, 'link_results' : link_results}, f)
-
 # --- Save CSV reports ---
 # swmm5.save_node_results('3125220_0')
 #swmm5.save_link_results('8748')
